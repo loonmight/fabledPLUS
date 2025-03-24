efb5a975324b1497bae540ba9d2a18576956f39b
loonmight <179328212+loonmight@users.noreply.github.com>
2025-03-24 22:14:49 +0100

    Fixed FlagMechanic
    Update FlagMechanic.java

--- src/main/java/studio/magemonkey/fabled/dynamic/mechanic/FlagMechanic.java
+++ src/main/java/studio/magemonkey/fabled/dynamic/mechanic/FlagMechanic.java
@@ -58,10 +58,10 @@ public class FlagMechanic extends MechanicComponent {
             return false;
         }
 
-        String key     = filter(caster, target, settings.getString(KEY));
         double seconds = parseValues(caster, SECONDS, level, 3.0);
-        int    ticks   = (int) (seconds * 20);
         for (LivingEntity target : targets) {
+			String key = filter(caster, target, settings.getString(KEY));
+			int ticks = (int) (seconds * 20);
             FlagManager.addFlag(target, key, ticks);
         }
         return targets.size() > 0;
