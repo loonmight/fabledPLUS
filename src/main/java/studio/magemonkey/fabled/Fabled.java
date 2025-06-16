e80be8a2458de339e031cb2706d42c3567b793ef
loonmight <179328212+loonmight@users.noreply.github.com>
2025-06-16 23:03:17 +0200

    InputChecker integration
    Update Fabled.java
    Registered InputChecker

--- src/main/java/studio/magemonkey/fabled/Fabled.java
+++ src/main/java/studio/magemonkey/fabled/Fabled.java
@@ -80,6 +80,7 @@ import studio.magemonkey.fabled.task.ManaTask;
 import studio.magemonkey.fabled.task.SaveTask;
 import studio.magemonkey.fabled.thread.MainThread;
 import studio.loonmight.emeraldglow.GlowWhileOnEmerald;
+import studio.magemonkey.fabled.dynamic.InputChecker;
 
 import java.io.File;
 import java.io.IOException;
@@ -667,6 +668,7 @@ public class Fabled extends SkillAPI {
         listen(new ExperienceListener(), settings.isYieldsEnabled());
         listen(new PluginChecker(), true);
 		Bukkit.getPluginManager().registerEvents(new GlowWhileOnEmerald(), this);
+		InputChecker.start(this);
 
         // Set up tasks
         if (settings.isManaEnabled()) {
